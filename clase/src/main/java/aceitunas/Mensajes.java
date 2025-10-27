c4c689fddbd8078cee01a5f3f4bc8de7d81defdd
PabloSL-Git <santamarialoredo@gmail.com>
2025-10-27 12:02:18 +0100

    mensaje inicial en clase mensaje

---
+++ clase/src/main/java/aceitunas/Mensajes.java
@@ -0,0 +1,19 @@
+package aceitunas;
+
+// varable publica, significa que es visible dessde cualquier clase
+//static, atribto de clase
+// final, valor constante (no cambie)
+
+public class Mensajes {
+    public static final String menuInicial = """
+            Bienbenido a almazara
+            No vuelvas
+            """;
+
+
+
+
+
+    
+
+}
