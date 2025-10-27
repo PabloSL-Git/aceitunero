29a10578cb03ce372d6a25992b937d142ae6852e
PabloSL-Git <santamarialoredo@gmail.com>
2025-10-27 12:14:05 +0100

    lectura inicial controlando exepciones

--- clase/src/main/java/aceitunas/Funciones.java
+++ clase/src/main/java/aceitunas/Funciones.java
@@ -6,8 +6,7 @@ public class Funciones {
     public static int pedirOpcion(){
         
         int opcion = 0;
-
-        String opcionString;
+        String opcionString = "";
 
         opcionString = JOptionPane.showInputDialog(Mensajes.menuInicial);
 
--- clase/src/main/java/aceitunas/Main.java
+++ clase/src/main/java/aceitunas/Main.java
@@ -2,17 +2,20 @@ package aceitunas;
 
 public class Main {
     public static void main(String[] args) {
-        
+
         int opcion = 0;
 
-
         do {
-            opcion = Funciones.pedirOpcion();
-            
+            try {
+
+                opcion = Funciones.pedirOpcion();
+
+            } catch (NumberFormatException nfe) {
+                // repite bucle al saltar exepcion
+                opcion = 0;
+            }
+
         } while (opcion != 2);
-
-
-
 
     }
 }
